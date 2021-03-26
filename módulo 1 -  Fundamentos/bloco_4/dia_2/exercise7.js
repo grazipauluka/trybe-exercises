eee6a283a3ff815837a0b13a0f75a74ea12006ff
Graziela Pauluka <grazipauluka@gmail.com>
2021-03-26 16:27:47 -0300

    salva exercises

--- módulo 1 -  Fundamentos/bloco_4/dia_2/exercise7.js
+++ módulo 1 -  Fundamentos/bloco_4/dia_2/exercise7.js
@@ -1,10 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior = [0];
+let menor = numbers[0];
 
 for (let index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] > maior) 
-    maior = numbers[index];
+  if (numbers[index] < menor) 
+    menor = numbers[index];
   }
 
-
-  console.log(maior);
+console.log(menor);
